27c81a2c650c6d9ff74f6ec5c613a94acd985deb
Csaba Schnitchen <schcsaba@gmail.com>
2023-03-31 05:50:21 +0200

    feat: add payments

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -11,7 +11,9 @@ router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', isAuth, shopController.getCart);
 router.post('/cart', isAuth, shopController.postCart);
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
-router.post('/create-order', isAuth, shopController.postOrder);
+router.get('/checkout', isAuth, shopController.getCheckout);
+router.get('/checkout/success', isAuth, shopController.getCheckoutSuccess);
+router.get('/checkout/cancel', isAuth, shopController.getCheckout);
 router.get('/orders', isAuth, shopController.getOrders);
 router.get('/orders/:orderId', isAuth, shopController.getInvoice);
 
